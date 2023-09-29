c44e7dbd592d0a82b909b0537b092300c47e5271
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-09-29 16:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 //your code here
-const parent = document.querySelector('#parent');
-const images = document.querySelectorAll('.image');
-console.log(parent , images)
-images.forEach(image =>{
-    console.log(image)
-    image.addEventListener('dragstart', ()=>{
-        image.classList.add('dragging');
-    })
-
-    image.addEventListener("dragend", ()=>{
-        image.classList.remove('dragging');
-    })
-})
-
-const rearrangeImage = (e) => {
-    e.preventDefault();
-    const currentDraggingElement = document.querySelector(".dragging");
-    const siblings = [...document.querySelectorAll('.image:not(.dragging)')]
-    // console.log(currentDraggingElement , siblings)
-    const nextSibling = siblings.find(sibling => {
-        // console.log(e.clientX , sibling.offsetLeft + (sibling.offsetWidth/2))
-        return e.clientX <= (sibling.offsetLeft )
-    })
-    // console.log(nextSibling);
-    parent.insertBefore(currentDraggingElement , nextSibling)
-    
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+ 
+const images = document.querySelectorAll(".image");
+ 
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
-
-parent.addEventListener('dragover', rearrangeImage);
-parent.addEventListener('dragenter', e =>  e.preventDefault())
+ 
+function allowDrop(e) {
+  e.preventDefault();
+}
+ 
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
+  }
+ 
+  dragdrop(clone);
+ 
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+ 
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
+}
+ 
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+ 
+images.forEach(dragdrop);
